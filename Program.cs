373a5e232f343eb78e940ef88929e8fbe706db5c
Benjamin <10912845+ben-har@users.noreply.github.com>
2020-07-06 10:56:45 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TcEventLoggerAdsProxyLib;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private const int LangId = 1031;
+        private static void Main()
+        {
+            var logger = new TcEventLogger();
+
+            try
+            {
+                logger.Connect(); //connect to localhost
+                Console.WriteLine("Connected...waiting for events");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to connect to event logger with exception:" + ex);
+            }
+
+            try
+            {
+                TcLoggedEventCollection e = logger.GetLoggedEvents(100);
+                int count = e.Count;
+
+                Console.WriteLine("Event log count: " + count);
+
+                if (logger.IsConnected && count != 0)
+                {
+                    // Call ClearAllAlarms first to ensure ClearLoggedEvents removes everything.
+                    logger.ClearAllAlarms();
+                    logger.ClearLoggedEvents();
+
+                    Console.WriteLine("Event log count after clear: " + e.Count);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Logger terminated with exception:" + ex);
+            }
+
+        }
+
+     }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# TwinCAT 3 Event Logger Clear
+
+## Disclaimer
+This is a personal guide not a peer reviewed journal or a sponsored publication. We make
+no representations as to accuracy, completeness, correctness, suitability, or validity of any
+information and will not be liable for any errors, omissions, or delays in this information or any
+losses injuries, or damages arising from its display or use. All information is provided on an as
+is basis. It is the reader’s responsibility to verify their own facts.
+
+The views and opinions expressed in this guide are those of the authors and do not
+necessarily reflect the official policy or position of any other agency, organization, employer or
+company. Assumptions made in the analysis are not reflective of the position of any entity
+other than the author(s) and, since we are critically thinking human beings, these views are
+always subject to change, revision, and rethinking at any time. Please do not hold us to them
+in perpetuity.
+
+## Overview 
+Demo showing how to clear the TwinCAT 3 Event Logger via C#.
+
+## Install 
+Not required. Simply open the project.
+
+## TwinCAT
+This project uses VS2017 Community.
+
+## Getting started
+Open the included project on your local machine
